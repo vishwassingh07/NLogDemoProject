4cecfa7aefb9d0f7f983f6c6bfc86f150241e2d6
Vishwas <baghel.vishwas0@gmail.com>
2022-04-20 00:36:13 +0530

    [Vishwas]add: Demo Of NLog Project

---
+++ NLogProject/AddNumbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogProject
+{
+    public class AddNumbers
+    {
+        private readonly NLog nLog = new NLog();
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            ////Checking values equal to 0
+            if (firstNumber == 0 || secondNumber == 0)
+            {
+                nLog.LogDebug("Debug Successfull : Sum()");
+                nLog.LogError("Expecting not null values");
+                nLog.LogWarn("FirstNumber or secondNumber should not equal to 0");
+            }
+
+            int result = secondNumber + secondNumber;
+            nLog.LogDebug("Debug Successfull : Sum()");
+            nLog.LogInfo("Sum Method passed, Result" + result);
+            return result;
+        }
+    }
+}
---
+++ NLogProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace NLogProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello This Is A NLog Demo");
+            AddNumbers numbers = new AddNumbers();
+            numbers.Sum(1, 2);
+        }
+    }
+}
